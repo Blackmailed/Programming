1f1345456ba471fb6b3c6b2cfd12b9bd1560be54
Danil D <kreptoID41@yandex.ru>
2022-03-09 17:50:16 +0700

    2

---
+++ src/Programming/Model/Manufacturers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public enum Manufacturers
+    {
+        Microsoft,
+        BlackBerry,
+        Huawei,
+        Google, 
+        Samsung,
+        Zte,
+        Honor
+    }
+}
