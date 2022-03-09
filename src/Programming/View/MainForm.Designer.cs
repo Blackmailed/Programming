93d2f1f4837243423ce90df34528d019b47cd135
Danil D <kreptoID41@yandex.ru>
2022-03-09 17:59:08 +0700

    3

--- src/Programming/View/MainForm.Designer.cs
+++ src/Programming/View/MainForm.Designer.cs
@@ -29,13 +29,49 @@ namespace Programming
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ProgrammingTabControl = new System.Windows.Forms.TabControl();
+            this.Enums = new System.Windows.Forms.TabPage();
+            this.ProgrammingTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ProgrammingTabControl
+            // 
+            this.ProgrammingTabControl.Controls.Add(this.Enums);
+            this.ProgrammingTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProgrammingTabControl.Location = new System.Drawing.Point(0, 0);
+            this.ProgrammingTabControl.Name = "ProgrammingTabControl";
+            this.ProgrammingTabControl.SelectedIndex = 0;
+            this.ProgrammingTabControl.Size = new System.Drawing.Size(800, 450);
+            this.ProgrammingTabControl.TabIndex = 0;
+            // 
+            // Enums
+            // 
+            this.Enums.Location = new System.Drawing.Point(4, 24);
+            this.Enums.Name = "Enums";
+            this.Enums.Padding = new System.Windows.Forms.Padding(3);
+            this.Enums.Size = new System.Drawing.Size(792, 422);
+            this.Enums.TabIndex = 0;
+            this.Enums.Text = "Enums";
+            this.Enums.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.ProgrammingTabControl);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Programming Demo";
+            this.ProgrammingTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl ProgrammingTabControl;
+        private System.Windows.Forms.TabPage Enums;
     }
 }
 
