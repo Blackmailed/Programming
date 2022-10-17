2d2a9c784f2ed3cd74d20d2e5495df826cde5b30
Danil D <kreptoID41@yandex.ru>
2022-10-17 17:04:53 +0700

    begining lab 2

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectOrientedPractics.Model
+{
+    public enum Category
+    {
+        Meat = 1,   //Мясной продукт  
+        Seafood,   //Морепродукт
+        DairyProguct,    //Молочный продукт
+        BakeryProduct,    //Выпечка
+        Cereal,    //Каша
+        Drink,    //Напиток
+        SideDishe    //Гарнир
+    }
+}
