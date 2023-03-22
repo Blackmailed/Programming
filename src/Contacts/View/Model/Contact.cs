764d7bc302a765f0ee1e642a58d8d475e2d51975
Danil D <kreptoID41@yandex.ru>
2023-03-22 20:19:04 +0700

    feature(Contact,MainWindow):Создал класс контакт, добавил конку в форму и название.

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    public class Contact
+    {
+        private string _name;
+
+        private string _phoneNumber;
+
+        private string _email;
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(string _name, string _phoneNumber, string _email)
+        {
+            Name = _name;
+            PhoneNumber = _phoneNumber;
+            Email = _email;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+    }
+}
