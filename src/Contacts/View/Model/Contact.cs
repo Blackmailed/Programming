e7031e50d7df5e60e3da35dc30fe1791f3b5c4e4
Danil D <kreptoID41@yandex.ru>
2023-04-19 19:19:54 +0700

    features(ContactSerializer, MainVM, RelayCommand):Добавил реализацию для серелизации и десерилизации, создал класс для главного окна,

--- src/Contacts/View/Model/Contact.cs
+++ src/Contacts/View/Model/Contact.cs
@@ -6,19 +6,40 @@ using System.Threading.Tasks;
 
 namespace View.Model
 {
+    /// <summary>
+    /// Хранит данные о контакте.
+    /// </summary>
     public class Contact
     {
+        /// <summary>
+        /// Хранит имя контакта.
+        /// </summary>
         private string _name;
 
+        /// <summary>
+        /// Хранит номер телефона контакта.
+        /// </summary>
         private string _phoneNumber;
 
+        /// <summary>
+        /// Хранит электронную почту контакта.
+        /// </summary>
         private string _email;
 
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>
+        /// </summary>
         public Contact()
         {
 
         }
 
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>
+        /// </summary>
+        /// <param name="_name">Имя контакта.</param>
+        /// <param name="_phoneNumber">Номер телефона контакта.</param>
+        /// <param name="_email">Адрес электронной почты контакта.</param>
         public Contact(string _name, string _phoneNumber, string _email)
         {
             Name = _name;
@@ -26,6 +47,9 @@ namespace View.Model
             Email = _email;
         }
 
+        /// <summary>
+        /// Возвращает и задает имя контакта.
+        /// </summary>
         public string Name
         {
             get
@@ -38,6 +62,9 @@ namespace View.Model
             }
         }
 
+        /// <summary>
+        /// Возвращает и задает номер телефона контакта.
+        /// </summary>
         public string PhoneNumber
         {
             get
@@ -50,6 +77,9 @@ namespace View.Model
             }
         }
 
+        /// <summary>
+        /// Возвращает и задает адрес электронной почты контакта.
+        /// </summary>
         public string Email
         {
             get
---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using View.Model;
+using View.Model.Services;
+
+namespace View.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Контакт.
+        /// </summary>
+        public Contact Contact { get; private set; } = new Contact();
+
+        /// <summary>
+        /// Возвращает и задаёт путь сериализации. По умолчанию - папка "Мои документы".
+        /// </summary>
+        public string Path { get; set; } =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            + @"\Contacts\contacts.json";
+
+        /// <summary>
+        /// Возвращает и задает имя контакта.
+        /// </summary>
+        public string Name
+        {
+            get
+            { 
+                return Contact.Name;
+            }
+            set
+            {
+                Contact.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает номер телефона контакта.
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                return Contact.PhoneNumber;
+            }
+            set
+            {
+                Contact.PhoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает адрес электронной почты контакта.
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return Contact.Email;
+            }
+            set
+            {
+                Contact.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        /// <summary>
+        /// Команда сериализации контакта.
+        /// </summary>
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+                    ContactSerializer.Serialize(Contact, Path);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Команда десериализации контакта.
+        /// </summary>
+        public ICommand LoadCommand
+        {
+            get => new RelayCommand((obj) =>
+            {
+                var contact = ContactSerializer.Deserialize(Path);
+                Name = contact.Name;
+                Email = contact.Email;
+                PhoneNumber = contact.PhoneNumber;
+            });
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        /// <summary>
+        /// Событие изменения свойства.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
