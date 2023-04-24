f8f9dc4decbef2ee76b1ad94b26a8fdab6eb87c6
Danil D <kreptoID41@yandex.ru>
2023-04-24 15:30:59 +0700

    fix

--- src/Contacts/View/Model/Contact.cs
+++ src/Contacts/View/Model/Contact.cs
@@ -1,31 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace View.Model
+﻿namespace View.Model
 {
     /// <summary>
     /// Хранит данные о контакте.
     /// </summary>
     public class Contact
     {
-        /// <summary>
-        /// Хранит имя контакта.
-        /// </summary>
-        private string _name;
-
-        /// <summary>
-        /// Хранит номер телефона контакта.
-        /// </summary>
-        private string _phoneNumber;
-
-        /// <summary>
-        /// Хранит электронную почту контакта.
-        /// </summary>
-        private string _email;
-
         /// <summary>
         /// Создает экземпляр класса <see cref="Contact"/>
         /// </summary>
@@ -37,59 +16,29 @@ namespace View.Model
         /// <summary>
         /// Создает экземпляр класса <see cref="Contact"/>
         /// </summary>
-        /// <param name="_name">Имя контакта.</param>
-        /// <param name="_phoneNumber">Номер телефона контакта.</param>
-        /// <param name="_email">Адрес электронной почты контакта.</param>
-        public Contact(string _name, string _phoneNumber, string _email)
+        /// <param name="name">Имя контакта.</param>
+        /// <param name="phoneNumber">Номер телефона контакта.</param>
+        /// <param name="email">Адрес электронной почты контакта.</param>
+        public Contact(string name, string phoneNumber, string email)
         {
-            Name = _name;
-            PhoneNumber = _phoneNumber;
-            Email = _email;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
         }
 
         /// <summary>
         /// Возвращает и задает имя контакта.
         /// </summary>
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                _name = value;
-            }
-        }
+        public string Name { get; set; }
 
         /// <summary>
         /// Возвращает и задает номер телефона контакта.
         /// </summary>
-        public string PhoneNumber
-        {
-            get
-            {
-                return _phoneNumber;
-            }
-            set
-            {
-                _phoneNumber = value;
-            }
-        }
+        public string PhoneNumber { get; set; }
 
         /// <summary>
         /// Возвращает и задает адрес электронной почты контакта.
         /// </summary>
-        public string Email
-        {
-            get
-            {
-                return _email;
-            }
-            set
-            {
-                _email = value;
-            }
-        }
+        public string Email { get; set; }
     }
 }
